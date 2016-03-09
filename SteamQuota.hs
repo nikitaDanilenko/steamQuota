0877fd2eb4c9a9797cba8c006d2a7c27ff0554ae
nikitaDanilenko <nda@informatik.uni-kiel.de>
2016-03-09 22:48:20 +0100

    Use monad transformer; fix achievement-free behaviour; add comments

--- SteamQuota.hs
+++ SteamQuota.hs
@@ -3,10 +3,12 @@ module SteamQuota where
 import Control.Applicative             ( (<$>), (<*>) )
 import Control.Arrow                   ( (&&&) )
 import Control.Monad                   ( MonadPlus ( mzero ), unless )
+import Control.Monad.Trans.Class       ( lift )
+import Control.Monad.Trans.Maybe       ( MaybeT, runMaybeT )
 
 import Data.Aeson                      ( FromJSON ( parseJSON ), Value ( Object ), (.:), decode, Object )
 import Data.Aeson.Types                ( Parser )
-import Data.ByteString.Lazy.Char8      ( pack, unpack, writeFile, readFile, hGetContents )
+import Data.ByteString.Lazy.Char8      ( pack, unpack, writeFile, readFile, ByteString )
 import Data.Char                       ( toLower )
 import Data.HashMap.Lazy               ( lookup )
 import Data.List                       ( sortBy, intercalate )
@@ -22,7 +24,6 @@ import Network.HTTP.Conduit            ( simpleHttp )
 import System.Directory                ( createDirectory, doesDirectoryExist, doesFileExist )
 import System.Environment              ( getArgs )
 import System.FilePath.Posix           ( pathSeparator )
-import System.IO                       ( openFile, IOMode ( ReadMode, WriteMode ), hClose )
 
 import Prelude hiding                  ( lookup, writeFile, readFile )
 import qualified Prelude as P          ( readFile )
@@ -35,49 +36,56 @@ type Id = Integer
 
 type PlayTime = Integer
 
+-- Shorthand for accessing fields in a JSON object.
+
 (.@) :: FromJSON a => Object -> String -> Parser a
 m .@ str = m .: Text.pack str
 
 -- A game consists of an ID and a play time value.
-	
+    
 data Game = Game { gameId :: Id, playTime :: PlayTime } deriving Show
 
 instance Eq Game where
 
     Game i _ == Game j _ = i == j
 
+-- Alien Swarm is a free game, whose game stats are bugged: due to the fact that it is free,
+-- apparently no user owns it, which is why it is added by hand
+
 alienSwarm :: Game
 alienSwarm = Game 630 100
 
 instance FromJSON Game where
 
-	parseJSON (Object m) = Game <$> m .@ "appid" <*> m .@ "playtime_forever"
-	parseJSON _          = mzero
+    parseJSON (Object m) = Game <$> m .@ "appid" <*> m .@ "playtime_forever"
+    parseJSON _          = mzero
 
 -- User games are represented as the number of games in total and a list of games.
-	
+    
 data UserGames = UserGames { totalGames :: Integer, -- number of games the user owns
                              gamesList :: [Game]    -- list of games the user owns
-						   }
-	deriving Show
+                           }
+    deriving Show
 
 instance FromJSON UserGames where
 
-	parseJSON (Object m) = UserGames <$> (m .@ "response" >>= (.@ "game_count"))
+    parseJSON (Object m) = UserGames <$> (m .@ "response" >>= (.@ "game_count"))
                                      <*> (m .@ "response" >>= (.@ "games"))
-	parseJSON _          = mzero
+    parseJSON _          = mzero
 
 -- An achievement consists of a name and a Boolean value whether it has been achieved yet or not.
-	
+    
 data Achievement = Achievement { achievementLabel :: String, -- name of the achievement
                                  achievementStatus :: Bool   -- achieved yet?
-							   }
-	deriving Show
+                               }
+    deriving Show
 
 instance FromJSON Achievement where
 
-	parseJSON (Object m) = Achievement <$> m .@ "name" <*> (toBool <$> (m .@ "achieved"))
-	parseJSON _          = mzero
+    parseJSON (Object m) = Achievement <$> m .@ "name" <*> (toBool <$> (m .@ "achieved"))
+    parseJSON _          = mzero
+
+-- Convert integers to Booleans.
 
 toBool :: Integer -> Bool
 toBool 0 = False
@@ -88,10 +96,12 @@ toBool _ = True
 
 data GameStats = GameStats { playerID :: Id,                -- stats for which player?
                              gameName :: String,            -- name of the game
-							 achievements :: [Achievement], -- list of achievements
+                             achievements :: [Achievement], -- list of achievements
                              achievable :: Int
-						   }
-	deriving Show
+                           }
+    deriving Show
+
+-- Set the number of possible achievements to the supplied value.
 
 setAchievable :: Int -> GameStats -> GameStats
 setAchievable n (GameStats p g a _) = GameStats p g a n
@@ -134,23 +144,37 @@ instance FromJSON FullGameStats where
                                          <*> (m .@ "game" >>= (.@ "availableGameStats") 
                                                           >>= (.@ "achievements"))
     parseJSON _          = mzero
-                                           
+
+-- Computes how many achievements have been achieved per game.
+                                 
 achievedPerGame :: GameStats -> Int
 achievedPerGame = length . filter achievementStatus . achievements
 
+-- Computes the quota (achieved/achievable) per game (double precision).
+
 quotaPerGame :: GameStats -> Double
 quotaPerGame gs = achievedPerGame gs * 100 // achievable gs
+
+-- Round a Double value after the supplied number of positions right of the decimal point.
+-- If the supplied position is less than zero, the original value is returned.
 
 roundAt :: Int -> Double -> Double
 roundAt pos d | pos >= 0  = round (10 ^ pos * d) // 10 ^ pos
               | otherwise = d
 
+-- Shorthand for dividing integer-like numbers and obtaining Doubles.
+
 infixl 1 //
 (//) :: Integral i => i -> i -> Double
 x // y = fromIntegral x / fromIntegral y
 
+-- Computes the average of a list of fractional values.
+
 average :: Fractional n => [n] -> n
-average xs = sum xs / fromIntegral (length xs)
+average = uncurry (/) . foldr (\x (s, l) -> (x + s, 1 + l)) (0, 0)
+
+-- Computes the numbers of achieved and achievable achievements per game in a list.
+-- A game counts towars the total value only if at least one achievement has been unlocked.
 
 toPrequotas :: [GameStats] -> [(Int, Int)]
 toPrequotas = map (achievedPerGame &&& achievable) . filter ((> 0) . quotaPerGame)
@@ -177,22 +201,22 @@ str .+ x = str ++ show x
 
 evaluate :: [GameStats] -> String
 evaluate gs = 
-	unlines [                
+    unlines [                
         "Total achieved                   : " .+ totalAchieved gs,
-	    "Total achievable                 : " .+ totalAchievements gs,
-		"Absolute quota                   : " .+ roundAt 3 (totalQuota gs),
-		"Average completion rate (exact)  : " .+ roundAt 3 (average qs),
-		"Average completion rate (biased) : " .+ roundAt 3 (average (quotasWith 0 gs)),
-		"",
-		"Perfect games                    : " .+ length g100,
-		"75% <= x < 100%                  : " .+ length g75,
-		"50% <= x < 75%                   : " .+ length g50,
-		"25% <= x < 50%                   : " .+ length g25,
-		"0%  <= x < 25%                   : " .+ length g0,
-		"",
-		"(Played) games with achievements : " .+ length qs,
-		"Sum of the above                 : " .+ sum (map length [g100, g75, g50, g25, g0])
-			 ]
+        "Total achievable                 : " .+ totalAchievements gs,
+        "Absolute quota                   : " .+ roundAt 3 (totalQuota gs),
+        "Average completion rate (exact)  : " .+ roundAt 3 (average qs),
+        "Average completion rate (biased) : " .+ roundAt 3 (average (quotasWith 0 gs)),
+        "",
+        "Perfect games                    : " .+ length g100,
+        "75% <= x < 100%                  : " .+ length g75,
+        "50% <= x < 75%                   : " .+ length g50,
+        "25% <= x < 50%                   : " .+ length g25,
+        "0%  <= x < 25%                   : " .+ length g0,
+        "",
+        "(Played) games with achievements : " .+ length qs,
+        "Sum of the above                 : " .+ sum (map length [g100, g75, g50, g25, g0])
+             ]
     where qs         = quotasWith (-1) gs
           ps         = sortBy (flip compare) qs
           (g100, r1) = span (== 100) ps
@@ -200,11 +224,21 @@ evaluate gs =
           (g50, r3)  = span (>= 50) r2
           (g25, g0)  = span (>= 25) r3
 
+{- These queries denote the prefixes of the Steam API pages that need to be queried to obtain
+   certain information:
+   ownedQuery: for querying the games a user owns
+   userStatsQuery: for querying individual game stats per user per game
+   schemaQuery: for querying game information with respect to achievements
+   recentQuery: querying the list of recently played games of a user.
+-}
+
 ownedQuery, userStatsQuery, schemaQuery, recentQuery :: String
 ownedQuery = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?"
 userStatsQuery = "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid="
 schemaQuery = "http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key="
 recentQuery = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?"
+
+-- Request a non-empty string until such a string has been provided.
 
 request :: String -> String -> IO String
 request rq text = do
@@ -213,15 +247,25 @@ request rq text = do
              >> getLine 
              >>= request rq
         else return text
-        
-keyRequest, userRequest, refreshRate :: String
-keyRequest  = "Please enter your Steam API key or save it to the file " ++ show keyFile ++ "."
-userRequest = "Please enter a Steam user id or save the default id to " ++ show userFile ++ "."
-refreshRate = "Please enter a refresh rate: r(ecent games only), g(ame stats), u(nlocked achievements), a(ll)"
+
+-- Request a key/user respectively, until some non-empty information has been supplied.
 
 requestKey, requestUser :: String -> IO String
 requestKey  = request keyRequest
 requestUser = request userRequest
+
+-- Request strings for the requests of keys, user ids, and refresh rates.
+
+keyRequest, userRequest, refreshRate :: String
+keyRequest  = "Please enter your Steam API key or save it to the file " ++ show keyFile ++ "."
+userRequest = "Please enter a Steam user id or save the default id to " ++ show userFile ++ "."
+refreshRate = intercalate "\n" [
+                       "Please enter a refresh rate:",
+                       "r(ecent): update stats on recently played games (up to ten) only",
+                       "g(ame stats): update game status only",
+                       "u(nlocked): update unlocked achievements only",
+                       "a(ll): update all information (may take a minute)"
+                      ]
 
 -- Requests a Steam API key and a user id from the user, until some valid information is obtained.
 
@@ -236,6 +280,104 @@ obtainKeyUser _                = do
                        else requestUser ""
     return (key, user)
         
+-- Map words beginning with the right letters to refresh rates.
+
+refreshFromString :: String -> Refresh
+refreshFromString ('r' : _) = Recent
+refreshFromString ('g' : _) = OnlyAchievable
+refreshFromString ('u' : _) = OnlyAchieved
+refreshFromString _         = All
+
+-- The directory in which the global game information is stored.
+                      
+gamesDirectory :: String
+gamesDirectory = "games"
+                   
+-- File that possibly contains the Steam API key.
+
+keyFile :: String
+keyFile = "keyfile"
+
+-- File that possibly contains the default user.
+
+userFile :: String
+userFile = "defaultuser"
+
+-- Check whether a directory exists and create it in the negative case.
+                      
+checkMakeDir :: String -> IO ()
+checkMakeDir dir = do b <- doesDirectoryExist dir
+                      unless b (createDirectory dir)
+
+-- Check whether the game directory exists or not and create it in the negative case.
+       
+checkGamesDirectory :: IO ()
+checkGamesDirectory = checkMakeDir gamesDirectory
+
+-- Creates a full path from a folder structure by intercalating the file separator.
+
+dirPath :: [String] -> String
+dirPath = intercalate [pathSeparator]
+
+-- A data type that represents, what information needs to be updated.
+
+data Refresh = Recent | OnlyAchieved | OnlyAchievable | All deriving Eq
+
+-- When is the local information sufficient with respect to games?
+
+keepGameLocal :: Refresh -> Bool
+keepGameLocal Recent       = True
+keepGameLocal OnlyAchieved = True
+keepGameLocal _            = False
+
+-- When is the local information sufficient with respect to users?
+
+keepUserLocal :: Refresh -> Bool
+keepUserLocal OnlyAchievable = True
+keepUserLocal _              = False
+
+-- Monadic if-then-else construct that either reads from the file supplied in the second
+-- argument (if the first one is True) or performs an external query using the query
+-- string in the third argument.
+
+readOrFetch :: Bool -> String -> String -> IO ByteString
+readOrFetch b local fetch | b         = readFile local
+                          | otherwise = simpleHttp fetch
+
+-- Action per game.
+-- This function checks whether any local data concerning the game or user exists.
+-- If it exists and only local data is required, the local data is read.
+-- If it does not exist or an update is required, the corresponding data is fetched
+-- and saved for later use.
+
+perGame :: String    -- ^ Steam API key
+        -> String    -- ^ Steam user id
+        -> Bool      -- ^ whether to read game stats locally (if possible)
+        -> Bool      -- ^ whether to read user stats locally (if possible)
+        -> Game      -- ^ game in question
+        -> MaybeT IO GameStats
+perGame key user gameLocal userLocal game = do
+    hasGame <- lift (doesFileExist gameFile)
+    hasUser <- lift (doesFileExist userFile)
+    let localGame = gameLocal && hasGame
+        localUser = userLocal && hasUser
+    gameStatus <- lift (readOrFetch localGame gameFile gameQuery)
+    lift (update localGame gameFile gameStatus)
+    case decode gameStatus :: Maybe FullGameStats of
+        Nothing -> mzero
+        Just fg -> do 
+            userStatus <- lift (readOrFetch localUser userFile userQuery)
+            lift (update localUser userFile userStatus)
+            maybe mzero (return . setAchievable (totalAchievements2 fg)) (decode userStatus)
+    where 
+        gameFile      = dirPath [gamesDirectory, gameLabel]
+        userFile      = dirPath [user, gameLabel]
+        update b file = unless b . writeFile file 
+        gameLabel     = show (gameId game)
+        gameQuery     = schemaQuery ++ key ++ "&appid=" ++ gameLabel ++ "&format=json"
+        userQuery     = userStatsQuery ++ gameLabel ++ "&key=" 
+                                       ++ key ++ "&steamid=" ++ user ++ "&format=json"
+
 main :: IO ()
 main = withSocketsDo $ do
     setLocaleEncoding utf8
@@ -256,82 +398,10 @@ main = withSocketsDo $ do
                                      && (g `elem` rs || refresh == Recent || refresh == OnlyAchieved))
                              gamesRecent
     case gamesOwned of
-        Just os -> mapM (\g -> perGame key user (keepGameLocal refresh) (updateUser g) g) os
+        Just os -> mapM (\g -> runMaybeT (perGame key 
+                                                  user 
+                                                  (keepGameLocal refresh) 
+                                                  (updateUser g) 
+                                                 g)) os
                               >>= putStrLn . evaluate . catMaybes
         _                  -> putStrLn ("Error while parsing: " ++ unpack owned ++ unpack recent)
-
-refreshFromString :: String -> Refresh
-refreshFromString ('r' : _) = Recent
-refreshFromString ('g' : _) = OnlyAchievable
-refreshFromString ('u' : _) = OnlyAchieved
-refreshFromString _         = All
-
--- The directory in which the global game information is stored.
-                      
-gamesDirectory :: String
-gamesDirectory = "games"
-                      
-keyFile :: String
-keyFile = "keyfile"
-
-userFile :: String
-userFile = "defaultuser"
-
--- Check whether a directory exists and create it in the negative case.
-                      
-checkMakeDir :: String -> IO ()
-checkMakeDir dir = do b <- doesDirectoryExist dir
-                      unless b (createDirectory dir)
-       
-checkGamesDirectory :: IO ()
-checkGamesDirectory = checkMakeDir gamesDirectory
-
--- Creates a full path from a folder structure by intercalating the file separator.
-
-dirPath :: [String] -> String
-dirPath = intercalate [pathSeparator]
-
--- A data type that represents, what information needs to be updated.
-
-data Refresh = Recent | OnlyAchieved | OnlyAchievable | All deriving Eq
-
-keepGameLocal :: Refresh -> Bool
-keepGameLocal Recent       = True
-keepGameLocal OnlyAchieved = True
-keepGameLocal _            = False
-
-keepUserLocal :: Refresh -> Bool
-keepUserLocal OnlyAchievable = True
-keepUserLocal _              = False
-
--- likely more elegant with a transformer.
-
-perGame :: String -> String -> Bool -> Bool -> Game -> IO (Maybe GameStats)
-perGame key user gameLocal userLocal game = do
-    hasGame <- doesFileExist gameFile
-    hasUser <- doesFileExist userFile
-    let localGame = gameLocal && hasGame
-        localUser = userLocal && hasUser
-    gameStatus <- if localGame
-                    then readFile gameFile
-                    else simpleHttp gameQuery 
-    let mgs = decode gameStatus :: Maybe FullGameStats
-    case mgs of
-        Nothing -> --writeFile gameFile gameStatus >> -- todo: proper closing required!
-                   return Nothing
-        Just fg -> do 
-            userStatus <- if localUser
-                            then readFile userFile
-                            else simpleHttp userQuery
-            let mstats = fmap (setAchievable (totalAchievements2 fg)) (decode userStatus)
-            T.sequence (fmap (\_ ->    update localGame gameFile gameStatus
-                                    >> update localUser userFile userStatus) mstats)
-            return mstats
-    where 
-        gameFile      = dirPath [gamesDirectory, gameLabel]
-        userFile      = dirPath [user, gameLabel]
-        update b file = unless b . writeFile file 
-        gameLabel     = show (gameId game)
-        gameQuery     = schemaQuery ++ key ++ "&appid=" ++ gameLabel ++ "&format=json"
-        userQuery     = userStatsQuery ++ gameLabel ++ "&key=" 
-                                       ++ key ++ "&steamid=" ++ user ++ "&format=json"
